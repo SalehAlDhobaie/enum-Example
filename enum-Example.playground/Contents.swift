47a0e1db488d426817cb276f56980e53799b3fb5
Saleh AlDhobaie <Saleh.Dhobaie@gmail.com>
2017-03-15 14:59:16 +0300

    - Prepare file.

---
+++ enum-Example.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+
+
+/**
+ * here I will write an exaple based on your sugesstion from blog post http://saleh.im/enum/
+ *
+ */
+
+

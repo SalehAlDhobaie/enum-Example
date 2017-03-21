cf3d6bd637056c85e12d751627ca10bd90cbed08
Saleh AlDhobaie <Saleh.Dhobaie@gmail.com>
2017-03-21 12:48:43 +0300

    - adding new example  from "https://twitter.com/hnosha15/status/843791412403589120"

--- enum-Example.playground/Contents.swift
+++ enum-Example.playground/Contents.swift
@@ -9,4 +9,50 @@ import Foundation
  *
  */
 
+public enum Season : String {
+    case winter, spring, summer, fall
+    static func randomSeason() -> Season {
+        
+        let season = [Season.winter, Season.spring, Season.summer, Season.fall]
+        let lower : UInt32 = 0
+        let upper : UInt32 = 3
+        let randomNumber = arc4random_uniform(upper - lower) + lower
+        return season[Int(randomNumber)]
+    }
+}
+
+extension Season : CustomStringConvertible {
+    public var description: String {
+        get {
+            switch self {
+            case .winter:
+                return "WINTER"
+            case .spring:
+                return "SPRING"
+            case .summer:
+                return "SUMMER"
+            case .fall:
+                return "FALL"
+            }
+        }
+    }
+}
+
+
+// assume the random generated season will be .summer else it's unExpected result..
+let summer = Season.randomSeason()
+switch summer {
+case .summer:
+    print("cool it's summer season!")
+default:
+    print("unExpected result")
+}
+
+
+
+
+
+
+
+
 
